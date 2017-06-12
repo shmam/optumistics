859deb8f02c4b9e0bb92ddb116f789dbb717427f
scrochet <samuel.crochet@optum.com>
2017-06-12 14:00:03 -0400

    deleted all the python code

--- backend/dataDumpTest.py
+++
@@ -1,4 +0,0 @@
-import simplejson as json
-from insertfunctions import *
-
-insert_Appointment_Type("blood draw", 30)
--- backend/testData.py
+++
@@ -1,8 +0,0 @@
-from insertFunctions import *
-
-def randomword(length):
-   return ''.join(random.choice(string.lowercase) for i in range(length))
-
-
-
-# NEED TO TEST PATIENT INFORMATION WITH NEW FOREIGN KEY RESTRAINT
