14df4439a0ae1ce77d8ad0064356ec4332812f8e
scrochet <samuel.crochet@optum.com>
2017-06-13 13:15:00 -0400

    merge conflict

--- testConnection.js
+++ testConnection.js
@@ -3,18 +3,24 @@ var config = require('./backend/config.js')
 var db = require('odbc')(), cn = 'DRIVER=' + config.driver + ';PORT=1433;SERVER='
     + config.server + ';PORT=1443;DATABASE=' + config.database + ';Trusted_Connection=yes';
 
-db.open(cn, function (err) {
-	if (err) return console.log(err);
-	  
 
-    db.query('SELECT * FROM Question', function (err, data) {
-        if (err) console.log(err);
+function testConnection(){
+    db.open(cn, function (err) {
+        if (err) return console.log(err);
+        
+
+        db.query('SELECT * FROM Question', function (err, data) {
+            if (err) console.log(err);
+        
+            console.log(data);
     
-        console.log(data);
- 
-        db.close(function () {
-            console.log('done');
+            db.close(function () {
+                console.log('done');
+            });
         });
     });
-});
-
+}
+ 
+module.exports = { 
+    testConnection
+}
