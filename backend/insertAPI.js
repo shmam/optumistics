ad3daa2a78972a6f243261189f95a665ce3afc7d
Yang <cyang112@LAMU02T78P3GTFM.uhc.com>
2017-06-14 10:06:16 -0400

    changes

--- backend/insertAPI.js
+++ backend/insertAPI.js
@@ -3,10 +3,10 @@ var config = require('./config.js')
 var db = require('odbc')(), cn = 'DRIVER=' + config.driver + ';PORT=1433;SERVER='
     + config.server + ';PORT=1443;DATABASE=' + config.database + ';Trusted_Connection=yes';
 
-
+//ALRIGHT
 //TESTED AND WORKS
 function insert_Appointment_Type(appointment_name, appointment_duration){
-	if(typeof appointment_name === 'string' && appointment_duration === null) {
+	if(typeof appointment_name === 'string' && appointment_duration == null) {
 		db.open(cn,function(err) {
 			if(err) return console.log(err);
 			db.query("INSERT INTO Appointment_Type VALUES ('" +appointment_name +"', null)", function(err) {
