e2c85e817ac39b2c23efadd387503b5d2b64ee54
Yang <cyang112@LAMU02T78P3GTFM.uhc.com>
2017-06-28 10:09:14 -0400

    changes

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -1,7 +1,7 @@
-  'use strict';
+'use strict';
 module.exports = function(app) {
   var insertAPI = require('../controller/postController.js');
-  var selectAPI = require('../controller/getController.js')
+  var selectAPI = require('../controller/getController.js');
 
   app.route('/portal/add/time/:provider_id/:action_id') // time for each doctor for each tasks (RT)
         .get(selectAPI.add_time_provider_task_rt)
