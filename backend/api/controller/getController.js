e8e38fe7fef492a6b0bad3e695c902ee738dc90d
jhayne14 <j.haynes@optum.com>
2017-07-11 17:33:56 -0400

    final try

--- backend/api/controller/getController.js
+++ backend/api/controller/getController.js
@@ -518,6 +518,7 @@ function loop(startcnt){
 					res.send(err);
 				}
 				else {
+					console.log("THIS IS DATA:   "+data1[0].time1-data1[0].time2)
 					return(data1[0].time1-data1[0].time2);  
 				}
 			});
