6019dd155330a8a43d720ba7a788bd57bb4f4dab
jhayne14 <j.haynes@optum.com>
2017-07-13 08:43:07 -0400

    start and end date

--- backend/api/controller/getController.js
+++ backend/api/controller/getController.js
@@ -136,18 +136,18 @@ function getTimeAllDoctorsDates_C(req,res){
 
 	   sync.fiber(function(){
 		   console.log("inside here");
-		for(var i=1;i<5;i++){
-			console.log("inside for  loop");
-			var start= req.params.start_date.substring(1,5)+"-"+String(i)+"-01"
-	   		var end= req.params.end_date.substring(1,5)+"-"+String(i)+"-31"
-			console.log("Start date"+start);
-			console.log("End date"+ end);
-			var data1 = sync.await(cn.query("SELECT AVG(TIMESTAMPDIFF(minute,start_time, end_time)) AS time FROM Action_Performed WHERE (action_date BETWEEN '"+start+"' AND '"+end+"') AND action_id = "+req.params.action_id, sync.defer()));
-			console.log("THIS IS THE REAL DATA: " +data1);
-			dataArr.push(data1);
+			for(var i=1;i<5;i++){
+				console.log("inside for  loop");
+				var start= req.params.start_date.substring(0,4)+"-"+String(i)+"-01"
+				var end= req.params.end_date.substring(0,4)+"-"+String(i)+"-31"
+				console.log("Start date"+start);
+				console.log("End date"+ end);
+				var data1 = sync.await(cn.query("SELECT AVG(TIMESTAMPDIFF(minute,start_time, end_time)) AS time FROM Action_Performed WHERE (action_date BETWEEN '"+start+"' AND '"+end+"') AND action_id = "+req.params.action_id, sync.defer()));
+				console.log("THIS IS THE REAL DATA: " +data1);
+				dataArr.push(data1);
 
-		}
-		res.jsonp(dataArr);
+			}
+			res.jsonp(dataArr);
 	});
 	
          
