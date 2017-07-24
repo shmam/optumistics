c2b275d1a2b933046a21c92867ebdfd9a8f5ed99
jhayne14 <j.haynes@optum.com>
2017-07-24 11:09:11 -0400

    cross origin

--- server.js
+++ server.js
@@ -82,6 +82,12 @@ app.get('/data_portal/optumImg', function(req, res) {
 var routes = require('./backend/api/routes/routes.js');
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 routes(app);
 
 
