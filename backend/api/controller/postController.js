48c60fd3c40c52bc71a9d16df9ee294b3b6174f0
jhayne14 <j.haynes@optum.com>
2017-07-25 10:23:29 -0400

    post comment

--- backend/api/controller/postController.js
+++ backend/api/controller/postController.js
@@ -520,25 +520,25 @@ function delete_Action(req, res) {
 	
 }
 
-function update_text_alert(req,res) {
-	cn.query("UPDATE Appointment SET text_alert=1 WHERE appointment_id="+ req.params.appointment_id, function(err,data1) {
-		if(err) {
-			console.log(err);
-			res.send(err);
-		}
-		else {
-			cn.query("UPDATE Patient_Information SET phone_number='"+req.params.phone_number+"' WHERE patient_id="+ req.params.patiend_id, function(err,data) {
-				if(err) {
-					console.log(err);
-					res.send(err);
-				}
-				else {
-					res.send("success");
-				}
-			});
-		}
-	});
-}
+// function update_text_alert(req,res) {
+// 	cn.query("UPDATE Appointment SET text_alert=1 WHERE appointment_id="+ req.params.appointment_id, function(err,data1) {
+// 		if(err) {
+// 			console.log(err);
+// 			res.send(err);
+// 		}
+// 		else {
+// 			cn.query("UPDATE Patient_Information SET phone_number='"+req.params.phone_number+"' WHERE patient_id="+ req.params.patiend_id, function(err,data) {
+// 				if(err) {
+// 					console.log(err);
+// 					res.send(err);
+// 				}
+// 				else {
+// 					res.send("success");
+// 				}
+// 			});
+// 		}
+// 	});
+// }
 
 
 module.exports = {
@@ -562,5 +562,5 @@ module.exports = {
 	update_Flag_Color,
 	end_appointment,
 	delete_Action,
-	update_text_alert
+	//update_text_alert
 }
