fed202d605fdc0d357e1ab0cb768d44720cc10f3
Yang <cyang112@LAMU02T78P3GTFM.uhc.com>
2017-07-26 15:16:24 -0400

    doc

--- data_portal/js/SettingsPage.js
+++ data_portal/js/SettingsPage.js
@@ -3,6 +3,7 @@ var action_clicked;
 var active_actions=[];
 var hex_arr=[];
 $(document).ready(function(){
+    alert(new Date());
 
     /* click button to open/close sidebar */
     $('#topBar-button').click(function () {
